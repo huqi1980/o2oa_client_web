6cf7b1b192feaf2fe7e7e9b255605cad8c997581
hq_1980@hotmail.com <hq_1980@hotmail.com>
2018-12-18 15:55:02 +0800

    upload x_desktop

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine'],
+        files: [
+            'test/**/*.js'
+        ],
+        exclude: [
+        ],
+        preprocessors: {
+            'source/**/*.js': ['coverage']
+        },
+
+        reporters: ['progress', 'coverage'],
+
+        coverageReporter: {
+            reporters: [
+                {type:'lcovonly', subdir: '.'},
+                {type:'json', subdir: '.'},
+                {type : 'html',dir : 'coverage/'}
+            ]
+        },
+
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: false,
+
+        browsers: ['Chrome'],
+
+        singleRun: true,
+        concurrency: Infinity
+    })
+}
